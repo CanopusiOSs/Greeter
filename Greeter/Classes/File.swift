744c6a5a9031bd013af5a32480a052c9b9dc1274
Nirmal <nirmal.patel@canopusinfosystems.com>
2023-03-16 17:22:13 +0530

    Final commit

---
+++ Greeter/Classes/File.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  Greeter
+//
+//  Created by IOS on 16/03/23.
+//
+
+import Foundation
+
+public class File {
+    public init() {}
+    public func greetWorld() -> String {
+        return "Hello world!"
+    }
+}
