a367936088bac4c688bbd76635ef13cfc221d5b4
Nirmal <nirmal.patel@canopusinfosystems.com>
2023-03-16 16:48:49 +0530

    Initial commit

---
+++ Example/Pods/Target Support Files/Greeter/Greeter-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Greeter : NSObject
+@end
+@implementation PodsDummy_Greeter
+@end
---
+++ Example/Pods/Target Support Files/Pods-Greeter_Example/Pods-Greeter_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Greeter_Example : NSObject
+@end
+@implementation PodsDummy_Pods_Greeter_Example
+@end
